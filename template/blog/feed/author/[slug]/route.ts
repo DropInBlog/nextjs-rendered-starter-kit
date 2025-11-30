71857331be8e7d0adab13aa86dd373e7475c2811
nidal1 <touhami9@gmail.com>
2025-11-30 19:18:45 +0100

    chore: update dependencies, error handling to feed routes and refactoring the template stucture.

--- template/blog/feed/author/[slug]/route.ts
+++ template/blog/feed/author/[slug]/route.ts
@@ -5,16 +5,24 @@ export async function GET(
   request: Request,
   { params }: { params: Promise<{ slug: string }> }
 ) {
-  const { slug } = await params;
+  try {
+    const { slug } = await params;
 
-  const data = await dibApi.fetchAuthorFeed({ slug });
+    const data = await dibApi.fetchAuthorFeed({ slug });
 
-  const headers: HeadersInit = {};
-  if (data.content_type) {
-    headers['Content-Type'] = data.content_type;
+    const headers: HeadersInit = {};
+    if (data.content_type) {
+      headers['Content-Type'] = data.content_type;
+    }
+
+    return new NextResponse(data.feed, {
+      headers,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Failed to fetch blog author feed' },
+      { status: 500 }
+    );
   }
 
-  return new NextResponse(data.feed, {
-    headers,
-  });
 }
--- template/blog/feed/route.ts
+++ template/blog/feed/route.ts
@@ -2,13 +2,21 @@ import { dibApi } from '../../../dib-lib/api';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const data = await dibApi.fetchBlogFeed();
-  const headers: HeadersInit = {};
-  if (data.content_type) {
-    headers['Content-Type'] = data.content_type;
+  try {
+    const data = await dibApi.fetchBlogFeed();
+    const headers: HeadersInit = {};
+    if (data.content_type) {
+      headers['Content-Type'] = data.content_type;
+    }
+
+    return new NextResponse(data.feed, {
+      headers,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Failed to fetch blog feed' },
+      { status: 500 }
+    );
   }
 
-  return new NextResponse(data.feed, {
-    headers,
-  });
 }
